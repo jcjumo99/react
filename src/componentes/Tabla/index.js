da82f21655034cbeba676ef8617517bcb2402411
Jhon <jdelgadol@autonoma.edu.pe>
2021-05-21 19:31:52 -0500

    gaaaa

--- src/componentes/Tabla/index.js
+++ src/componentes/Tabla/index.js
@@ -9,56 +9,50 @@ console.log(numbers);
 function Tabla(){
  // const [mascotas, setMascotas] = useState([
     const mascotas = [
-    {
-    tipo: "gato",
-    nombre: "manchas",
-    dueno: "esteban",
-  },
-  {
-    tipo: "gataa",
-    nombre: "manchasss",
-    dueno: "estebanss",
-  }
-  
-];
+      {
+        tipo: "gato",
+        nombre: "manchas",
+        dueno: "esteban",
+      },
+      {
+        tipo: "gataa",
+        nombre: "manchasss",
+        dueno: "estebanss",
+      }
+    ];
  
     return(
        //<div className="container">
         <table className="table  table-hover">
-        <thead className="table-dark">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Nombre</th>
-              <th scope="col">Tipo de Mascota</th>
-              <th scope="col">Dueños</th>
-              <th scope="col"></th>
-            </tr>
-        </thead>
+          <thead className="table-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Tipo de Mascota</th>
+                <th scope="col">Dueños</th>
+                <th scope="col"></th>
+              </tr>
+          </thead>
               <tbody id="lista-mascotas">
-                {" as"}
-               {mascotas.map(
-                 (mascota, index) => { 
-                
-      <tr>
-      <th scope="row">{index}</th>
-      <td>{mascota.nombre}</td>
-      <td>{mascota.tipo}</td>
-      <td>{mascota.dueno}</td>
-      <td>
-      <div className="btn-group" role="group" aria-label="Basic example"> 
-        <button type="button" className="btn btn-danger eliminar">
-          <i className="fas fa-trash-alt"></i></button>
-        <button type="button" className="btn btn-info editar" 
-        data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-          <i className="fas fa-pencil-alt"></i></button>
-      </div>
-      </td>
-    </tr>
-    })}         
-        </tbody>
-        </table>
-     
-    )
+                {mascotas.map( (mascota, index) => (
+                  <tr>
+                    <th scope="row">{index}</th>
+                    <td>{mascota.nombre}</td>
+                    <td>{mascota.tipo}</td>
+                    <td>{mascota.dueno}</td>
+                    <td>
+                      <div className="btn-group" role="group" aria-label="Basic example"> 
+                        <button type="button" className="btn btn-danger eliminar">
+                          <i className="fas fa-trash-alt"></i></button>
+                        <button type="button" className="btn btn-info editar" 
+                        data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                          <i className="fas fa-pencil-alt"></i></button>
+                      </div>
+                  </td>
+                </tr>
+                ))}         
+          </tbody>
+        </table>)
 }
 //console.log(mascotas);
  
